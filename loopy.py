1aca097474e6acf77dd09ac3755ebabcc747535e
Kelly Klingler <kbrieklingler@gmail.com>
2019-10-18 17:42:32 -0700

    Adding script from class

---
+++ loopy.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+NewVar = '1746589937616'
+LengthNewVar = len(NewVar)
+print('Length of the string is:', LengthNewVar)
+
+if LengthNewVar >= 3:
+	Number2 = NewVar.count('2')
+	print ("The number of 2's is:", Number2)
+
+else: 
+	print('NewVar')
